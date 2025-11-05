71735c40b4776596c84e6399ebabf6dabe176cd3
alexirie <alex_gayan@hotmail.com>
2025-11-05 19:24:47 +0100

    arreglo

--- server/server/src/main/java/com/example/server/util/Constantes.java
+++ server/server/src/main/java/com/example/server/util/Constantes.java
@@ -2,7 +2,7 @@ package com.example.server.util;
 
 public class Constantes {
 
-    public static final String IMAGES_URL = 'https://freak-project-production.up.railway.app/uploads/';
+    public static final String IMAGES_URL = "https://freak-project-production.up.railway.app/uploads/";
 
     private Constantes() {
         
