540be167dd38c2488e0578f08006e7b2415779ec
alexirie <alex_gayan@hotmail.com>
2025-11-06 21:49:29 +0100

    cambiada ruta al proxy

--- client/src/constantes.js
+++ client/src/constantes.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://freak-project-production.up.railway.app/api';
+export const BASE_URL = 'https://lively-integrity-production-f343.up.railway.app/api';
 
 export const IMAGES_URL = 'https://freak-project-production.up.railway.app/uploads/';
