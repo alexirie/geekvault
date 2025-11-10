b14a344eff1ad09b5afae80e741f45664d94a25a
alexirie <alex_gayan@hotmail.com>
2025-11-10 23:12:37 +0100

    subida al front para comprobar btn atras

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate   } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import FigureDetail from './pages/FigureDetail'; 
+import FigureDetail from './pages/FigureDetail';
+import { App as CapacitorApp } from "@capacitor/app"; 
+
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    CapacitorApp.addListener("backButton", () => {
+      // Si estás en la página principal, salir; si no, navegar atrás
+      if (window.location.pathname === "/") {
+        CapacitorApp.exitApp(); // salir de la app
+      } else {
+        navigate(-1);
+      }
+    });
+  }, [navigate]);
+
   return (
     <Router>
       <Routes>
