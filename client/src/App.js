b45d774d18db666688a2ea3fc4272d2a2693b72b
alexirie <alex_gayan@hotmail.com>
2025-11-10 23:23:19 +0100

    subida comprobar

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,38 @@
 // src/App.js
-import {useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate   } from 'react-router-dom';
+import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import FigureDetail from './pages/FigureDetail';
-import { App as CapacitorApp } from "@capacitor/app"; 
+import { App as CapacitorApp } from "@capacitor/app";
 
 
 function App() {
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    CapacitorApp.addListener("backButton", () => {
-      // Si estás en la página principal, salir; si no, navegar atrás
-      if (window.location.pathname === "/") {
-        CapacitorApp.exitApp(); // salir de la app
-      } else {
-        navigate(-1);
-      }
-    });
-  }, [navigate]);
+  function BackButtonHandler() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      CapacitorApp.addListener("backButton", () => {
+        if (window.location.pathname === "/") {
+          CapacitorApp.exitApp(); // salir de la app
+        } else {
+          navigate(-1); // navegar atrás
+        }
+      });
+    }, [navigate]);
+
+    return null; // este componente no renderiza nada
+  }
 
   return (
     <Router>
+      {/* Manejador del botón atrás dentro del Router */}
+      <BackButtonHandler />
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
         {/* Otras rutas */}
-        <Route path="/figure/:id" element={<FigureDetail />}/>
+        <Route path="/figure/:id" element={<FigureDetail />} />
       </Routes>
     </Router>
   );
