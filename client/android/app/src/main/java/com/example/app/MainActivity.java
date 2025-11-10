cb8fb2d1aa4a065b7208d970d1be41fcaca12ca9
alexirie <alex_gayan@hotmail.com>
2025-11-11 00:05:48 +0100

    nueva apk

--- client/android/app/src/main/java/com/example/app/MainActivity.java
+++ client/android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,17 @@
 package com.example.app;
 
+import android.view.View;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Desactivar efecto rebote de la WebView
+        this.bridge.getWebView().setOverScrollMode(View.OVER_SCROLL_NEVER);
+
+    }
+}
