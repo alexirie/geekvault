6739ee88681489f6cdaf1523e83fd7e0c6d6f23e
alexirie <alex_gayan@hotmail.com>
2025-11-14 17:34:02 +0100

    Añadir transaccional en refreshToken

--- server/server/src/main/java/com/example/server/auth/RefreshTokenRepository.java
+++ server/server/src/main/java/com/example/server/auth/RefreshTokenRepository.java
@@ -1,11 +1,16 @@
 package com.example.server.auth;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 import com.example.server.model.User;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
   Optional<RefreshToken> findByToken(String token);
+
+  @Transactional
+  @Modifying
   void deleteByUser(User user);
 }
