18e8dc11e520d70fb502684c40dbed0068d0a460
alexirie <alex_gayan@hotmail.com>
2025-11-16 20:49:28 +0100

    añadido docker y cambiadas rutas al nuevo back

--- client/src/constantes.js
+++ client/src/constantes.js
@@ -2,8 +2,10 @@
 //export const BASE_URL = 'https://lively-integrity-production-f343.up.railway.app/api';
 
 //local
-export const BASE_URL = 'https://freak-project-production.up.railway.app/api';
+//export const BASE_URL = 'https://freak-project-production.up.railway.app/api';
+export const BASE_URL = 'https://backend-269332876576.europe-west1.run.app/api';
 
-export const BASE_URL_L = 'https://freak-project-production.up.railway.app';
+//export const BASE_URL_L = 'https://freak-project-production.up.railway.app'; 
+export const BASE_URL_L = 'https://backend-269332876576.europe-west1.run.app';
 
-export const IMAGES_URL = 'https://freak-project-production.up.railway.app/uploads/';
+export const IMAGES_URL = 'https://backend-269332876576.europe-west1.run.app/uploads/';
